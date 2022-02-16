f95bb1654332e1ae0ac23a30b7fcd22b33c93c9a
Rebace <Rebace@mail.ru>
2022-02-16 20:21:38 +0300

    1

---
+++ DZ_ Ellipse/Ellipse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Ellipse
+{
+    public class Ellipse
+    {
+        public int VerticalRadius { get; private set; }
+        public int HorizontalRadius { get; private set; }
+        public Ellipse(int verticalRadius, int horizontalRadius)
+        {
+            if (verticalRadius <= 0 | horizontalRadius <= 0)
+            {
+                throw new ArgumentException("Enter a radius greater than zero.");
+            }
+            VerticalRadius = verticalRadius;
+            HorizontalRadius = horizontalRadius;
+        }
+
+        public double GetSquare()
+        {
+            return Math.Round(Math.PI * VerticalRadius * HorizontalRadius, 2);
+        }
+        public double GetLength()
+        {
+            return Math.Round(Math.PI * (VerticalRadius + HorizontalRadius), 2);
+        }
+    }
+}
---
+++ DZ_ Ellipse/Program.cs
@@ -0,0 +1,21 @@
+﻿using DZ_Ellipse;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int verticalRadius = 10;
+        int horizontalRadius = 5;
+
+        try
+        {
+            Ellipse ellipse = new Ellipse(verticalRadius, horizontalRadius);
+            Console.WriteLine($"Площадь эллипса: {ellipse.GetSquare()}");
+            Console.WriteLine($"Длина эллипса: {ellipse.GetLength()}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
